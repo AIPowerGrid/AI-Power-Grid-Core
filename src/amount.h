98a11a8824879203e3a472ef6f4f93461bccfbb6
AI Power Grid <admin@aipowergrid.io>
2024-01-18 09:37:44 -0500

    Fixing max supply to 200m

--- src/amount.h
+++ src/amount.h
@@ -13,7 +13,7 @@
 /** Amount in corbies (Can be negative) */
 typedef int64_t CAmount;
 
-static const CAmount COIN = 200000000;
+static const CAmount COIN = 100000000;
 static const CAmount CENT = 1000000;
 
 /** No amount larger than this (in satoshi) is valid.
@@ -25,7 +25,7 @@ static const CAmount CENT = 1000000;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 21000000000 * COIN;
+static const CAmount MAX_MONEY = 200000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif //  AIPG_AMOUNT_H

598a65ac3ab231e86b680f0cba8c64c22590ee43
blockchainbulls <justin@techtoaster.com>
2024-05-09 10:14:17 -0400

    Changed QT wallet Splashscreen loading message.

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -193,7 +193,7 @@ static void InitMessage(SplashScreen *splash, const std::string &message)
     QMetaObject::invokeMethod(splash, "showMessage",
         Qt::QueuedConnection,
         Q_ARG(QString, QString::fromStdString(message)),
-        Q_ARG(int, Qt::AlignBottom|Qt::AlignHCenter),
+        Q_ARG(int, Qt::AlignTop|Qt::AlignHCenter),
         Q_ARG(QColor, QColor(255,255,255)));
 }
 
